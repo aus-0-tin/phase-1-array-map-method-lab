61f70bbcfc31bce60513f6706aabd90a40436fc8
aus-0-tin <austin.waithaka@student.moringaschool.com>
2024-03-29 20:31:50 +0300

    completes lab

--- index.js
+++ index.js
@@ -1,3 +1,18 @@
+function titleCased() {
+  return tutorials.map(title => {
+  
+    const words = title.split(' ');
+
+    const capitalizedWords = words.map(word => {
+      return word.charAt(0).toUpperCase() + word.slice(1);
+    });
+
+    
+    return capitalizedWords.join(' ');
+  });
+}
+
+
 const tutorials = [
   'what does the this keyword mean?',
   'What is the Constructor OO pattern?',
@@ -9,8 +24,20 @@ const tutorials = [
   'what is the difference between == and ===?',
   'what is the difference between event capturing and bubbling?',
   'what is JSONP?'
+  "what does the this keyword mean?",
+  "What is the Constructor OO pattern?",
+  "implementing Blockchain Web API",
+  "The Test Driven Development Workflow",
+  "What is NaN and how Can we Check for it",
+  "What is the difference between stopPropagation and preventDefault?",
+  "Immutable State and Pure Functions",
+  "what is the difference between == and ===?",
+  "what is the difference between event capturing and bubbling?",
+  "what is JSONP?",
 ];
 
 const titleCased = () => {
   return tutorials
 }
+// Test function
+console.log(titleCased());
